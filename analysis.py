c71b10c8cace05dd74bb36f31aa1808e991fe27b
Tatjana <g00425678@atu.ie>
2023-05-03 19:03:30 +0200

    comments added to analysispy file

---
+++ analysis.py
@@ -0,0 +1,6 @@
+# Analysis.py
+# This program performs a basic exploratory data analysis (EDA) on the Fisher's Iris data set.
+# Outputs a summary of each variable 
+# Saves a histogram of each variable to a PNG file
+# Outputs a scatter plot of each pair of variables
+# Author: Tatjana Staunton
